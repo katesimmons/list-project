1f5d795b01d7a6adc2e2a6961fb12cf0c1c21d5e
Kate Simmons <kate.a.simmons@gmail.com>
2015-08-01 12:22:16 -0500

    add and delete working, only partial to do v done state change success

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
 	$('li.to-do').click(function(e) {
 		$(this).toggleClass('to-do').toggleClass('done');
-	})
-	$('li.done').click(function() {
+	});
+	$('li.done').click(function(e) {
 		$(this).toggleClass('done').toggleClass('to-do');
-	})
+	});
+	$('#btn').click(function(){
+    var text = $('#itemInput').val() + '<button>Delete</button>';
+    if(text.length){
+        $('<li />', {html: text}).appendTo('ul.list').addClass('to-do');
+    }
+});
+	$('ul').on('click','button' , function(el){
+    $(this).parent().remove();
+});
 });
 
-function addListItem () {
-    var x = document.getElementById("itemInput").value;
-    document.getElementById("demo").innerHTML = x;
-}
+
+//get value 
+//function addListItem () {
+//    var x = document.getElementById("itemInput").value;
+//    document.getElementById("demo").innerHTML = x;
+//}
+
+
+
+//function myFunction() {
+//    var node = document.createElement("LI");
+//    var textnode = document.createTextNode("Water");
+//    node.appendChild(textnode);
+//    document.getElementById("myList").appendChild(node);
+//}
